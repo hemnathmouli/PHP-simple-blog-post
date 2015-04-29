24cced333039af476f2a0fb778e43e8e4751e454
hemnathmouli <hemnathmouli@gmail.com>
2015-04-29 22:55:09 +0530

    logout.php
    You will be logged out here

---
+++ logout.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Logout</title>
+</head>
+
+<body>
+<a href="login.php">Login</a> and post again.<br />
+Or see the <a href="index.php">Posts</a>.
+</body>
+</html>
